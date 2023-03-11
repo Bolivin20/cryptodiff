474be54b9e8dbb2f2080c192c744b644ddfa3bef
Nikodem Zbaraza <nikozet@interia.pl>
2023-03-11 11:08:14 +0100

    initial commit

---
+++ src/main/java/com/cryptodiff/CryptodiffApplication.java
@@ -0,0 +1,13 @@
+package com.cryptodiff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptodiffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptodiffApplication.class, args);
+    }
+
+}
