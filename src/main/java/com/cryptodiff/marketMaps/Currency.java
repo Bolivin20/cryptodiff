00e250f06446a4ce9049b6b42bec3ded37d3bd67
Anna Zielińska <lananahb@gmail.com>
2023-03-12 15:20:48 +0100

    added binance market logic

---
+++ src/main/java/com/cryptodiff/marketMaps/Currency.java
@@ -0,0 +1,30 @@
+package com.example.zespolowy;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Currency {
+    String symbol;
+    Double price;
+
+
+    public Currency() {
+    }
+
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+}
