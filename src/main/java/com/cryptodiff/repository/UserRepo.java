13a9d2c3301438f19aa335ba287180e7a0975fd4
Nikodem Zbaraza <nikozet@interia.pl>
2023-04-04 17:08:23 +0200

    added user entity and repo, added dockerfile and compose

---
+++ src/main/java/com/cryptodiff/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package com.cryptodiff.repository;
+
+import com.cryptodiff.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+}
