96cd8cc1292ed1529a8ec6e3dfd53ce3db17903f
Nikodem Zbaraza <nikozet@interia.pl>
2023-04-17 12:47:50 +0200

    removed

--- src/main/java/com/cryptodiff/controller/MainController.java
+++ src/main/java/com/cryptodiff/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.cryptodiff.controller;
 
-import com.cryptodiff.entity.Crypto;
 import com.cryptodiff.marketMaps.BitstampMarket;
 import com.cryptodiff.marketMaps.HuobiMarket;
 import com.cryptodiff.repository.CryptoRepo;
@@ -8,9 +7,6 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 @RestController
 public class MainController {
@@ -38,43 +34,5 @@ public class MainController {
         Gson gson = new Gson();
         String json = gson.toJson(bitstampMarket.getMarketPrices());
         return json;
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        createCryptoRecords();
-        return "Initiated";
-    }
-    //ta operacja byłaby wykonana tylko raz na init danych w tabeli bazy danych, potem juz tylko by były updateowane rekordy
-    private void createCryptoRecords() {
-        HashMap<String, Double> huobiPrices = huobiMarket.getMarketPrices();
-        Iterator<Map.Entry<String, Double>> it = huobiPrices.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, Double> entry = it.next();
-            String symbol = entry.getKey();
-            double price = entry.getValue();
-            cryptoRepo.save(new Crypto(symbol, price));
-        }
-    }
-
-    @GetMapping("/update")
-    public String update() {
-        //docelowo update dla wszystkich giełd
-        updateCryptoRecordsForHuobi();
-        Gson gson = new Gson();
-        return gson.toJson(cryptoRepo.findAll());
-    }
-
-    //TODO zrobic 2 getMappingi które zwraca najlepsza cene i gielde dla kupna i sprzedazy (chyba ze optymaniej bedzie jak front to wykona)
-
-    private void updateCryptoRecordsForHuobi() {
-        HashMap<String, Double> huobiPrices = huobiMarket.getMarketPrices();
-        Iterator<Map.Entry<String, Double>> it = huobiPrices.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, Double> entry = it.next();
-            String symbol = entry.getKey();
-            double price = entry.getValue();
-            cryptoRepo.updateCrypto(symbol, price);
-        }
     }
 }
