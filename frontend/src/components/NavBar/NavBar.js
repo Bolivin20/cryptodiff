2b46ab2848ae4ba3b282a6613f308cdc98d0383a
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2023-04-19 18:03:05 +0200

    basic linking

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,16 +1,17 @@
 import style from './NavBar.module.css';
 import Logo from '../Logo/Logo';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function NavBar() {
 
     return (
         <div className={style.menuBar}>
-            <Logo fontSize='2em' height='50em' />
+            <Link className={style.logoLink} to='/'><Logo fontSize='2em' height='50em' /></Link>
             <div className={style.tabs}>
-                <p>Ranking</p>
-                <p>test@crypto.diff</p>
+            <Link className={style.logoLink} to='/'><p>Ranking</p></Link>
+            <Link className={style.logoLink} to='/settings'><p>test@crypto.diff</p></Link>
                 <p>Log Out</p>
             </div>
         </div>
