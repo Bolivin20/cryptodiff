ada16dd4487ebc98306243000e7c59ab6e35f125
Anna Zielińska <lananahb@gmail.com>
2023-05-15 14:41:21 +0200

    added posibility to show all markets

--- frontend/src/components/CryptoLabel/CryptoLabel.js
+++ frontend/src/components/CryptoLabel/CryptoLabel.js
@@ -1,11 +1,10 @@
 import style from './CryptoLabel.module.css';
-import React from 'react';
 import EmptyStar from '../../images/empty_star.svg';
 import Binance from '../../images/Binance.svg';
 import Huobi from '../../images/Huobi.svg';
 import Bitstamp from '../../images/Bitstamp.svg';
 import Arrow from '../../images/Arrow.svg';
-
+import React, { useState } from 'react';
 function provideCorrectIcon(market) {
     if (market === "bitstamp") {
         return Bitstamp;
@@ -19,22 +18,42 @@ function provideCorrectIcon(market) {
 
 function CryptoLabel(props) {
     const {symbol, pricesMap} = props;
-    const [market, price] = Object.entries(pricesMap)[0];
-    const capitalized = market.charAt(0).toUpperCase() + market.slice(1);
-    const marketIcon = provideCorrectIcon(market);
-
+    const [showPrices, setShowPrices] = useState(false);
+    const exchanges = Object.keys(pricesMap);
+   // const marketIcon = provideCorrectIcon(market);
+    const prices = Object.values(pricesMap);
 
     return (
+      <div className='market-container'>
         <div className={style.label}>
             <img src={EmptyStar} alt="star-icon"/>
             <p>{symbol.toUpperCase()}</p>
             <div className={style.market}>
-                <img src={marketIcon} alt="market-icon"/>
-                <p>{capitalized}</p>
+                <img src={provideCorrectIcon(exchanges[0])} alt="market-icon"/>
+                <p>{exchanges[0]}</p>
             </div>
-            <p>${price}</p>
-            <img src={Arrow} alt="arrow-icon" style={{height: "2.1em"}}/>
+            <p>${prices[0]}</p>
+            <img src={Arrow} alt="arrow-icon" style={{height: "2.1em"}} 
+            onClick={() => { setShowPrices(!showPrices); }}/>
         </div>
+          {
+            showPrices && (
+              <div>
+                {exchanges.slice(1).map((e) => { 
+                  return (
+                    <div className={style.label}>
+                      <p>{symbol.toUpperCase()}</p>
+                      <div className={style.market}>
+                        <p>{e}</p>
+                      </div>
+                      <p>{pricesMap[e]}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            )
+          }
+          </div>
     );
 }
 
