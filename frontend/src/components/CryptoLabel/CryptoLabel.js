69e36a27aa82e86629e440d377cd032de91a3102
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2023-05-26 14:00:22 +0200

    f aligned components and fixed bug with label rolled by index

--- frontend/src/components/CryptoLabel/CryptoLabel.js
+++ frontend/src/components/CryptoLabel/CryptoLabel.js
@@ -1,3 +1,5 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import style from './CryptoLabel.module.css';
 import EmptyStar from '../../images/empty_star.svg';
 import FilledStar from '../../images/filled_star.svg';
@@ -5,28 +7,22 @@ import Binance from '../../images/Binance.svg';
 import Huobi from '../../images/Huobi.svg';
 import Bitstamp from '../../images/Bitstamp.svg';
 import Arrow from '../../images/Arrow.svg';
-import React, {useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 
-
-function provideCorrectIcon(market) {
-    if (market === 'bitstamp') {
-        return Bitstamp;
-    } else if (market === 'huobi') {
-        return Huobi;
-    } else if (market === 'binance') {
-        return Binance;
-    }
-}
+const markets = {
+    bitstamp: Bitstamp,
+    huobi: Huobi,
+    binance: Binance,
+};
 
 function CryptoLabel(props) {
     const {symbol, pricesMap} = props;
     const [showPrices, setShowPrices] = useState(false);
     const [starClicked, setStarClicked] = useState(false);
-    const exchanges = Object.keys(pricesMap);
-    const prices = Object.values(pricesMap);
     const [jwtToken, setJwtToken] = useState('');
     const navigate = useNavigate();
+    const exchanges = Object.keys(pricesMap);
+    const prices = Object.values(pricesMap);
 
     // useEffect(() => {
     //     const token = localStorage.getItem('token');
@@ -38,55 +34,37 @@ function CryptoLabel(props) {
 
     const handleStarClick = () => {
         setStarClicked(!starClicked);
-        if (!starClicked) {
-            const requestData = {
-                symbol: symbol,
-            };
-            fetch('http://localhost:8080/user/api/add', {
-                method: 'POST',
+        const requestData = {
+            symbol: symbol,
+        };
+        const endpoint = starClicked ? 'delete' : 'add';
+        axios
+            .post(`http://localhost:8080/user/api/${endpoint}`, requestData, {
                 headers: {
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${jwtToken}`,
                 },
-                body: JSON.stringify(requestData),
             })
-                .then((response) => {
-                    if (response.ok) {
-                        console.log('Symbol dodany pomyślnie.');
-                    } else {
-                        console.log('Wystąpił błąd podczas dodawania symbolu.');
-                        console.log(response);
-                    }
-                })
-                .catch((error) => {
-                    console.log('Wystąpił błąd podczas wysyłania żądania.');
-                });
-        } else {
-            fetch('http://localhost:8080/user/api/delete', {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${jwtToken}`,
-                },
-                body: JSON.stringify({symbol: symbol}),
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log(`Symbol ${endpoint === 'add' ? 'dodany' : 'usunięty'} pomyślnie.`);
+                } else {
+                    console.log(`Wystąpił błąd podczas ${endpoint === 'add' ? 'dodawania' : 'usuwanie'} symbolu.`);
+                    console.log(response);
+                }
             })
-                .then((response) => {
-                    if (response.ok) {
-                        console.log('Subskrypcja usunięta pomyślnie.');
-                    } else {
-                        console.log('Wystąpił błąd podczas usuwania subskrypcji.');
-                        console.log(symbol);
-                    }
-                })
-                .catch((error) => {
-                    console.log('Wystąpił błąd podczas wysyłania żądania.');
-                    console.log(error);
-                });
-        }
+            .catch((error) => {
+                console.log('Wystąpił błąd podczas wysyłania żądania.');
+                console.log(error);
+            });
     };
 
     const capitalizeFirstLetter = (string) => {
         return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+    };
+
+    const handleArrowClick = () => {
+        setShowPrices(!showPrices);
     };
 
     return (
@@ -99,25 +77,23 @@ function CryptoLabel(props) {
                 />
                 <p>{symbol.toUpperCase()}</p>
                 <div className={style.market}>
-                    <img src={provideCorrectIcon(exchanges[0])} alt="market-icon"/>
+                    <img src={markets[exchanges[0]]} alt="market-icon"/>
                     <p>{capitalizeFirstLetter(exchanges[0])}</p>
                 </div>
                 <p>${prices[0]}</p>
                 <img
                     src={Arrow}
                     alt="arrow-icon"
-                    style={{height: '2.1em', gridColumnStart:6, gridColumnEnd: 7}}
-                    onClick={() => {
-                        setShowPrices(!showPrices);
-                    }}
+                    className={showPrices ? style.arrowRotated : ''}
+                    onClick={handleArrowClick}
                 />
             </div>
             {showPrices && (
                 <div>
                     {exchanges.slice(1).map((e) => (
-                        <div className={style.extraLabel}>
+                        <div className={style.label} key={e}>
                             <div className={style.market}>
-                                <img src={provideCorrectIcon(e)} alt="market-icon"/>
+                                <img src={markets[e]} alt="market-icon"/>
                                 <p>{capitalizeFirstLetter(e)}</p>
                             </div>
                             <p>${pricesMap[e]}</p>
@@ -130,4 +106,3 @@ function CryptoLabel(props) {
 }
 
 export default CryptoLabel;
-
--- frontend/src/views/Start/Start.js
+++ frontend/src/views/Start/Start.js
@@ -6,72 +6,85 @@ import EmptyStar from '../../images/empty_star.svg';
 import Page from '../../components/Page/Page';
 import CryptoLabel from '../../components/CryptoLabel/CryptoLabel';
 import Arrow from '../../images/Arrow.svg';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function Start() {
-  const [cryptoData, setCryptoData] = useState([]);
-  const [allData, setAllData] = useState([]);
-  const [selectedOption, setSelectedOption] = useState('asc');
+    const [cryptoData, setCryptoData] = useState([]);
+    const [allData, setAllData] = useState([]);
+    const [selectedOption, setSelectedOption] = useState('asc');
+    const [searchValue, setSearchValue] = useState('');
 
-  const handleSearch = async (event) => {
-      const filteredData = allData.filter(item => item.symbol.toLowerCase().includes(event.target.value.toLowerCase()));
-      setCryptoData(filteredData);
-      console.log(filteredData);
+    const handleSearch = (event) => {
+        const value = event.target.value;
+        setSearchValue(value);
     };
 
-  useEffect(() => {
-    async function fetchCryptoData() {
-      const response = await fetch(`http://localhost:8080/api/prices/${selectedOption === 'asc' ? 'asc' : 'desc'}`);
-      const data = await response.json();
-      setCryptoData(data);
-      setAllData(data);
-      console.log(data);
+    useEffect(() => {
+        async function fetchCryptoData() {
+            try {
+                const response = await axios.get(`http://localhost:8080/api/prices/${selectedOption === 'asc' ? 'asc' : 'desc'}`);
+                const data = response.data;
+                setAllData(data);
+                console.log(data);
+            } catch (error) {
+                console.log('Wystąpił błąd podczas pobierania danych.');
+                console.log(error);
+            }
+        }
+
+        fetchCryptoData();
+    }, [selectedOption]);
+
+    useEffect(() => {
+        const filteredData = allData.filter((item) =>
+            item.symbol.toLowerCase().includes(searchValue.toLowerCase())
+        );
+        setCryptoData(filteredData);
+    }, [searchValue, allData]);
+
+    function handleBuyButtonClick() {
+        setSelectedOption('asc');
     }
 
-    fetchCryptoData();
-  }, [selectedOption]);
+    function handleSellButtonClick() {
+        setSelectedOption('desc');
+    }
 
-  function handleBuyButtonClick() {
-    setSelectedOption('asc');
-  }
-
-  function handleSellButtonClick() {
-    setSelectedOption('desc');
-  }
-
-  return (
-    <Page>
-      <div className={style.searchBar}>
-          <div className={style.category}>
-              <p onClick={handleBuyButtonClick}>Buy</p>
-              <hr style={{visibility: selectedOption === 'asc' ? 'visible' : 'hidden'}}/>
-          </div>
-          <div className={style.category}>
-              <p onClick={handleSellButtonClick}>Sell</p>
-              <hr style={{visibility: selectedOption === 'desc' ? 'visible' : 'hidden'}}/>
-          </div>
-        <Input placeholder='search...' type='text' inputIcon={Lupa} onChange={handleSearch} width='30%'></Input>
-      </div>
-      <div className={style.content}>
-        <Box>
-          <div className={style.tableLable}>
-            <img src={EmptyStar} alt="star-icon"></img>
-            <p>Cryptocurrency</p>
-            <p>Stock Market</p>
-            <p>Buy Price</p>
-            <img src={Arrow} alt="arrow-icon" style={{visibility: 'hidden', height: "2.1em"}}></img>
-          </div>
-            {cryptoData.map((crypto, index) => (
-              <CryptoLabel
-                key={index}
-                symbol={crypto.symbol}
-                pricesMap={crypto.pricesMap}
-              />
-            ))}
-        </Box>
-      </div>
-    </Page>
-  );
+    return (
+        <Page>
+            <div className={style.searchBar}>
+                <div className={style.category}>
+                    <p onClick={handleBuyButtonClick}>Buy</p>
+                    <hr style={{visibility: selectedOption === 'asc' ? 'visible' : 'hidden'}}/>
+                </div>
+                <div className={style.category}>
+                    <p onClick={handleSellButtonClick}>Sell</p>
+                    <hr style={{visibility: selectedOption === 'desc' ? 'visible' : 'hidden'}}/>
+                </div>
+                <Input placeholder='search...' type='text' inputIcon={Lupa} onChange={handleSearch} value={searchValue}
+                       width='30%'/>
+            </div>
+            <div className={style.content}>
+                <Box>
+                    <div className={style.tableLable}>
+                        <img src={EmptyStar} alt="star-icon"/>
+                        <p>Cryptocurrency</p>
+                        <p>Stock Market</p>
+                        <p>Buy Price</p>
+                        <img src={Arrow} alt="arrow-icon" style={{visibility: 'hidden', height: "2.1em"}}/>
+                    </div>
+                    {cryptoData.map((crypto) => (
+                        <CryptoLabel
+                            key={crypto.symbol}
+                            symbol={crypto.symbol}
+                            pricesMap={crypto.pricesMap}
+                        />
+                    ))}
+                </Box>
+            </div>
+        </Page>
+    );
 }
 
 export default Start;
