7e4e7443e18e8726545f9ae2a18fbf2b247e20fd
Nikodem Zbaraza <nikozet@interia.pl>
2023-05-28 21:01:36 +0200

    added token removing on logout button

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -3,6 +3,10 @@ import Logo from '../Logo/Logo';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function handleLogout() {
+    localStorage.removeItem('token');
+    window.location.reload();
+}
 
 function NavBar() {
 
@@ -12,7 +16,7 @@ function NavBar() {
             <div className={style.tabs}>
             <Link className={style.logoLink} to='/'><p>Ranking</p></Link>
             <Link className={style.logoLink} to='/settings'><p>test@crypto.diff</p></Link>
-                <p>Log Out</p>
+            <Link onClick={handleLogout} className={style.logoLink}><p>Log out</p></Link>
             </div>
         </div>
     );
