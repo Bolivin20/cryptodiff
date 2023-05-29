3b7e971859182f327139b05d25a295fe664759ab
Nikodem Zbaraza <nikozet@interia.pl>
2023-05-29 10:19:17 +0200

    set request method type

--- src/main/java/com/cryptodiff/controller/MarketController.java
+++ src/main/java/com/cryptodiff/controller/MarketController.java
@@ -3,15 +3,12 @@ package com.cryptodiff.controller;
 import com.cryptodiff.marketMaps.Currency;
 import com.cryptodiff.marketMaps.MarketsManager;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", method = { RequestMethod.GET})
 @Component
 @CrossOrigin
 public class MarketController {
